60760b4b015739278321b2eb6171c8d2c1837280
Greg Magarshak <greg@qbix.com>
2024-10-16 02:39:39 -0400

    Fixed saving transcript at the right path

--- handlers/AI/webhook/transcription/index.php
+++ handlers/AI/webhook/transcription/index.php
@@ -4,6 +4,10 @@ function AI_webhook_transcription_index($params)
 {
     $fetched = Q::event("AI/webhook/transcription/fetch/".$params['platform']);
     $basename = Q::ifset($_REQUEST, 'basename', basename($fetched['audio_url']));
+    $path = APP_FILES_DIR . DS . 'AI' . DS . 'transcriptions';
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
     file_put_contents($path . DS . "$basename.transcript", json_encode($fetched));
     $lines = '';
     foreach ($fetched['utterances'] as $u) {
@@ -36,10 +40,6 @@ function AI_webhook_transcription_index($params)
         $result .= "[$hms]"
             . "\n" . json_encode($results['keywords'])
             . "\n" . $results['summary'] . PHP_EOL . PHP_EOL;
-    }
-    $path = APP_FILES_DIR . DS . 'AI' . DS . 'transcriptions';
-    if (!file_exists($path)) {
-        mkdir($path, 0777, true);
     }
     file_put_contents($path . DS . "$basename.summary", $result);
 }
